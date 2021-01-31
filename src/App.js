e0bd52b20ac4a80e53e2abe1894a7d6f1096d343
LeninFabricio <66141598+FabricioY19@users.noreply.github.com>
2021-01-31 06:45:22 +0100

    Correccion
    Corrección de las rutas, instalación de las dependencias : react, react-router, styled-componente- browser-react-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Navbar from '../src/Vistas/components/Navbar';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from '../src/Vistas/pages/home';
+import Hombres from '../src/Vistas/pages/hombres';
+import Informacion from '../src/Vistas/pages/informacion';
+import Mujeres from '../src/Vistas/pages/mujeres';
+import Novedades from '../src/Vistas/pages/novedades';
+import Catalogo from '../src/Vistas/pages/catalogo';
+import Accesorios from '../src/Vistas/pages/accesorios';
+import Carrito from '../src/Vistas/pages/carrito';
+import Micuenta from '../src/Vistas/pages/micuenta';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <Router>
+      <Navbar/>
+        <Switch>
+        <Route path='/Home' exact component={Home} />
+        <Route path='/hombres' exact component={Hombres} />
+        <Route path='/mujeres' exact component={Mujeres} />
+        <Route path='/novedades' exact component={Novedades} />
+        <Route path='/informacion' exact component={Informacion} />
+        <Route path='/catalogo' exact component={Catalogo }/>
+        <Route path='/accesorios' exact component={Accesorios}/>
+        <Route path='/carrito' exact component={Carrito} />
+        <Route path='/micuenta' exact component={Micuenta} />
+        </Switch>
+    </Router>
   );
+  
 }
 
 export default App;
