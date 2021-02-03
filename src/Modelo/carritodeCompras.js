d87f365c27b02777cff7d415326d20ac605db6a1
LeninFabricio <66141598+FabricioY19@users.noreply.github.com>
2021-02-03 16:52:56 +0100

    Base CrudProducto , Base carritoCompra
    Crud producto:Crear,Modificar,Buscar,Dar de Baja
    CarritoC: agregaralCarro, quitardelCarro

---
+++ src/Modelo/carritodeCompras.js
@@ -0,0 +1,6 @@
+
+class CarritoProducto{
+    constructor(){
+       
+    }
+}
---
+++ src/Modelo/producto.js
@@ -0,0 +1,13 @@
+
+var ID;
+var nombreProducto;
+var descripcionProducto;
+var precioProducto;
+class Producto{
+    constructor(ID, nombreProducto, descripcionProducto, precioProducto){
+        this.ID=ID;
+        this.nombreProducto=nombreProducto;
+        this.descripcionProducto= descripcionProducto;
+        this.precioProducto=precioProducto;
+    }
+}
